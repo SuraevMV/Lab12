59d37d2e33b289c61c87ac2e73335b964f5b0789
Михаил <stulstoldevyat@gmail.com>
2022-02-21 23:20:41 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12
+{
+    static class Circle
+    {
+
+        static public double DlinaCircle(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        static public double SquareCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        static public  bool ProverkaTochki(double x0, double y0, double x1, double y1, double radius)
+        {
+            if (Math.Pow((x1 - x0), 2) + Math.Pow((y1 - y0), 2) == Math.Pow(radius, 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные: х0, y0, x1, y1, R");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double dlina = Circle.DlinaCircle(radius);
+            double square = Circle.SquareCircle(radius);
+            Console.WriteLine("Длина окружности равна {0}\nПлощадь окружности {1}", dlina, square);
+            bool proverka;
+            if (Circle.ProverkaTochki(x0, y0, x1, y1, radius) == true)
+            {
+                Console.WriteLine("Точка принадлежит поверхности");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит поверхности");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
